435ebd12f2f1bbc93de5eef014f4d33ea4ef60b6
Isak Strömberg <isak.stromberg@gmail.com>
2016-04-13 18:09:29 +0200

    lagt till lite exempelgrafer i grafpanelen

--- Java-kod/RobotControlApplication/src/view/GraphPanel.java
+++ Java-kod/RobotControlApplication/src/view/GraphPanel.java
@@ -2,6 +2,7 @@ package view;
 
 import java.awt.Dimension;
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -10,28 +11,60 @@ import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class GraphPanel extends JPanel{
 
 	private JTextArea graphPlaceHolder;
-	
-	private ArrayList<Integer> graphList;
+
+	private ArrayList<ChartPanel> chartList;
+
+	private String[] chartNames = {"IR F/R", "IR F/L", "IR B/R",
+									"IR B/L", "Lidar Lite", "Angular velocity",
+									"Angle to wall"};
+	private int chartNameCounter = 0;
+
+	private String[] chartUnits = {"cm", "cm", "cm",
+									"cm", "cm", "deg/s",
+									"deg"};
 
 	public GraphPanel() {
-		graphList = new ArrayList<>();
-		
-//		JFreeChart lineChart = ChartFactory.createLineChart("Test chart", "Index", "Number", createDataset(), 
-//				PlotOrientation.VERTICAL, true, true, false);
-//		
-//		chartPanel = new ChartPanel(lineChart);
-//		chartPanel.setPreferredSize(new Dimension(560, 367));
-//		frame.add(chartPanel);
-		
-//		graphPlaceHolder = new JTextArea(7,40);
-//		graphPlaceHolder.setEditable(false);
-//		graphPlaceHolder.setText("This is a placeholder for the graphs");
-//
-//		add(graphPlaceHolder);
+		chartList = new ArrayList<>();
+
+		JFreeChart iRXYChart = ChartFactory.createXYLineChart("IR", "Time [s]", "Distance [cm]", createRandomDataset(4, 4, 30));
+		JFreeChart lidarXYChart = ChartFactory.createXYLineChart("Lidar", "Time [s]", "Distance [cm]", createRandomDataset(1, 0, 600));
+		JFreeChart angVelXYChart = ChartFactory.createXYLineChart("Angular velocity", "Time [s]", "Ang.vel [deg/s]", createRandomDataset(1, 0, 300));
+		JFreeChart angXYChart = ChartFactory.createXYLineChart("Angle to wall", "Time [s]", "Angle [deg]", createRandomDataset(1, 0, 15));
+
+		chartList.add(new ChartPanel(iRXYChart));
+		chartList.add(new ChartPanel(lidarXYChart));
+		chartList.add(new ChartPanel(angVelXYChart));
+		chartList.add(new ChartPanel(angXYChart));
+
+		for(ChartPanel chart : chartList) {
+			chart.setPreferredSize(new Dimension(300, 200));
+			add(chart);
+		}
 	}
 
+	// dummy procedure for creating dataset
+	private XYDataset createRandomDataset(int numberOfSets, double min, double max) {
+		XYSeriesCollection dataset = new XYSeriesCollection();
+		
+		for(int i = 0; i < numberOfSets; i++) {
+			XYSeries series = new XYSeries(chartNames[chartNameCounter++]);
+			
+			for(int j = 0; j < 10; j++) {
+				series.add(j, ThreadLocalRandom.current().nextDouble(min, max));
+			}
+			
+			dataset.addSeries(series);
+		}
+
+		return dataset;
+
+	}
 }
