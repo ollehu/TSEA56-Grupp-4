9eb45040ac17b172056584a1ad0cf9d15f1920df
Elena Tronje <Elena Tronje>
2016-04-13 17:34:24 +0200

    I2C-bussen

--- C-kod/Styrmodul/I2C_styr.c
+++ C-kod/Styrmodul/I2C_styr.c
@@ -5,15 +5,6 @@
  *  Author: eletr654
  */ 
 
-
-/*
- * Styrmodul.cpp
- *
- * Created: 4/11/2016 2:17:07 PM
- *  Author: eletr654
- */ 
-
-
 /*
 * Slave.c
 *
@@ -26,20 +17,19 @@
 #define F_CPU 16000000UL
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include "LCD.h"
 
-int sensorData[24];
+int sensorData[25];
 int styrKommando;
 
 ISR(TWI_vect){
 	TWCR = (1<<TWEA)|(1<<TWEN)|(0<<TWIE);
 	PORTA = (0<<PORTA0);
 	
-	int counter = 0;
+	int ind = 0;	
 	int komID;
 	
 	while(1){
-		
-		
 		//SLAVE RECEIVER
 		if((TWSR & 0xF8) == 0x60){
 			//SLA_W received, ACK returned
@@ -48,22 +38,21 @@ ISR(TWI_vect){
 			TWCR &= ~(1<<TWSTO);
 		} else if ((TWSR & 0xF8) == 0x80){
 			//SLA_W, ACK returned, wait for data
-			TWCR |= (1<<TWINT)|(1<<TWEA)|(1<<TWEN);
 			
-			if (counter == 0){
+			if (ind == 0){
 				komID = TWDR;
-				PORTB = komID;
-				counter = counter + 1;
+				ind = ind + 1;
 			} else {
 				if (komID == 0){
-					styrKommando = TWDR;
-					PORTB = styrKommando;
+					styrKommando = TWDR;					
+				} else if (komID ==2) {
+					sensorData[ind] = TWDR;
+					ind = ind + 1;
 				} else {
-					sensorData[counter-1] = TWDR;
-					PORTB = sensorData[counter-1];
-					counter = counter + 1;
+					//Incorrect com.ID, what to do?
 				}
 			}
+			TWCR |= (1<<TWINT)|(1<<TWEA)|(1<<TWEN);
 		} else if ((TWSR & 0xF8) == 0x80){
 			//SLA_W, NOT ACK returned
 			
@@ -88,7 +77,7 @@ ISR(TWI_vect){
 			//SLA_R, last byte transmitted
 			TWCR |= (1<<TWINT)|(1<<TWEA)|(1<<TWIE);
 			break;
-		} 
+		}
 		
 		while(!(TWCR & (1<<TWINT)));
 	}
@@ -97,14 +86,50 @@ ISR(TWI_vect){
 void TWISetup()
 {
 	//Set slave address and start TWI (including the TWI-interrupt)
-	TWAR = (1<<TWA6)|(1<<TWA5)|(0<<TWA4)|(0<<TWA3)|(1<<TWA2)|(0<<TWA1)|(0<<TWA0)|(0<<TWGCE);
+	TWAR = (1<<TWA6)|(1<<TWA5)|(0<<TWA4)|(0<<TWA3)|(1<<TWA2)|(1<<TWA1)|(0<<TWA0)|(0<<TWGCE);
 	TWCR |= (1<<TWEA)|(1<<TWEN)|(1<<TWIE);
 }
 
 int main(void)
 {
-	DDRB = 0xFF;
+	initLCD();
+	lcdWriteTopRow("Hej hopp");
 	TWISetup();
 	sei();
-	while(1);
+	//while(1);
+	DDRB = (1<<PORTB4);
+	_delay_ms(3000);
+	PORTB = (1<<PORTB4);
+	_delay_ms(10);
+	PORTB = (0<<PORTB4);
+		
+	switch (styrKommando)
+	{
+	case 0:
+		//Reglering av/på
+		lcdWriteTopRow("Reglering");
+		break;
+	case 1:
+		//Kör framåt
+		lcdWriteTopRow("Framat");
+		break;
+	case 2:
+		//Kör bakåt
+		lcdWriteTopRow("Bakat");
+		break;
+	case 3:
+		//Rotera åt höger
+		lcdWriteTopRow("Rotera");
+		lcdWriteBottomRow("hoger");
+		break;
+	case 4:
+		//Rotera åt vänster
+		lcdWriteTopRow("Rotera");
+		lcdWriteBottomRow("vanster");
+		break;
+	case 5:
+		//Gripklo öppna/stänga
+		lcdWriteTopRow("Gripklo");
+		break;
+	}
 }
